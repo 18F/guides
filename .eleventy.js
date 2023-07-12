0a029f9280d2462ac2afe25980bf0c7280061f3b
LoganMMcDonald <logan.mcdonald@gsa.gov>
2023-07-12 12:51:15 -0700

    pass through all js

--- .eleventy.js
+++ .eleventy.js
@@ -22,8 +22,7 @@ module.exports = function (config) {
   config.addPassthroughCopy({'./node_modules/@uswds/uswds/dist/js/uswds-init.js': 'assets/js/uswds-init.js'});
 
   // Specific scripts to guides
-  config.addPassthroughCopy("./assets/_common/js/flashing.js");
-  config.addPassthroughCopy("./assets/_common/js/time-outs.js");
+  config.addPassthroughCopy("./assets/_common/js/*");
   //
   config.addPassthroughCopy({'./assets/_common/_img/favicons/favicon.ico': './favicon.ico' });
   config.addPassthroughCopy({'./assets/_common/_img/favicons': './img/favicons' });
