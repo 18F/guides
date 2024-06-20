6869638dc9fc13ad26ee37d83d5e8e746e9b6692
Caley Woods <caley.woods@gsa.gov>
2024-06-20 09:56:29 -0500

    Update h2 highlighting code

--- assets/_common/js/app.js
+++ assets/_common/js/app.js
@@ -1,37 +1,52 @@
 require('@uswds/uswds');
 
-const sidenav = document.querySelector('.usa-sidenav');
-
-window.addEventListener('scroll', setCurrentLink);
-
+/* eslint no-plusplus: 0 */
 /**
  * Find the most recently passed heading and adds a usa-current
  * class to the correspoing link in the sidenav subnav
  */
-function setCurrentLink(){
-  if (sidenav) {
-    const h2s = document.querySelectorAll('h2');
-    const scrollPos = document.documentElement.scrollTop;
-    let topHead = h2s[0];
-    let i = 0;
-    let found = false;
-    while (!found && i < h2s.length) {
-        if (scrollPos < h2s[i].offsetTop){
-          found = true;
-        } else {
-          topHead = h2s[i];
-        }
-        i++;
-    }
-
-    const href = topHead.id;
-    const oldLink = document.querySelector('.usa-sidenav__sublist .usa-current');
-
-    if (oldLink) {
-      oldLink.classList.remove('usa-current');
-    }
-
-    const currentLink = document.querySelector(`.usa-sidenav__sublist [href="#${href}"]`).parentElement;
-    currentLink.classList.add('usa-current');
+function setCurrentLink() {
+  if (!document.querySelector('.usa-sidenav')) {
+    return;
   }
+  if (!document.querySelector('.usa-sidenav__sublist')) {
+    return;
+  }
+
+  const h2s = document.querySelectorAll('h2');
+  if (h2s.length <= 1) {
+    return;
+  }
+
+  const scrollPos = document.documentElement.scrollTop;
+  let topHead = h2s[0];
+  let i = 0;
+  let found = false;
+  while (!found && i < h2s.length) {
+    if (scrollPos < h2s[i].offsetTop) {
+      found = true;
+    } else {
+      topHead = h2s[i];
+    }
+    i++;
+  }
+  const href = topHead.id;
+  const oldLink = document.querySelector('.usa-sidenav__sublist .usa-current');
+  if (oldLink) {
+    oldLink.classList.remove('usa-current');
+  }
+  const currentLink = document.querySelector(
+    `.usa-sidenav__sublist [href='#${href}']`,
+  ).parentElement;
+  currentLink.classList.add('usa-current');
+}
+
+/**
+ * Add the event listener to find the nearest heading on user scroll
+ */
+if (
+  document.querySelector('.usa-sidenav')
+    && document.querySelectorAll('h2').length > 1
+) {
+  window.addEventListener('scroll', setCurrentLink);
 }
