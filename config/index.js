e7374e9fc623466b72a490ac437f8328a230e01c
Nate Borrebach <nate.borrebach@gsa.gov>
2024-09-18 16:18:09 -0700

    Refactor: extract image URL helper functions from imageWithClassShortcode

--- config/index.js
+++ config/index.js
@@ -1,17 +1,7 @@
 const path = require('path');
 const Image = require('@11ty/eleventy-img');
 
-async function imageWithClassShortcode(
-  src,
-  cls,
-  alt,
-) {
-  let pathPrefix = '';
-
-  if (process.env.BASEURL) {
-    pathPrefix = process.env.BASEURL;
-  }
-
+async function imageData(src) {
   const ext = path.extname(src);
   const fileType = ext.replace('.', '');
 
@@ -21,6 +11,25 @@ async function imageWithClassShortcode(
   });
 
   const data = metadata[fileType] ? metadata[fileType][0] : metadata.jpeg[0];
+  return data;
+}
+
+function getPathPrefix() {
+  let pathPrefix = '';
+
+  if (process.env.BASEURL) {
+    pathPrefix = process.env.BASEURL;
+  }
+  return pathPrefix;
+}
+
+async function imageWithClassShortcode(
+  src,
+  cls,
+  alt,
+) {
+  const pathPrefix = getPathPrefix();
+  const data = await imageData(src);
   return `<img src="${pathPrefix}${data.url}" class="${cls}" alt="${alt}" loading="lazy" decoding="async">`;
 }
 
